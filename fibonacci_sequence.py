bb00930f31ca5602fc5dac394fc56ef9e8bc4397
soapkenobi <soap@mexpert.eu.org>
2024-09-09 16:34:37 +0530

    rebase

--- fibonacci_sequence.py
+++
@@ -1,6 +0,0 @@
-terms = int(input())
-fib = [0, 1]
-for i in range(2, terms):
-    fib.append(fib[i-1] + fib[i-2])
-for i in fib:
-    print(i, end = ' ' if i != fib[len(fib) - 1] else '\n')
--- selection_sort.py
+++
@@ -1,8 +0,0 @@
-arr = list(eval(input()))
-for i in range(len(arr)):
-    min = i
-    for j in range(i, len(arr)):
-        if arr[j]<=arr[min]:
-            min = j
-    arr[min], arr[i] = arr[i], arr[min]
-print(arr)
